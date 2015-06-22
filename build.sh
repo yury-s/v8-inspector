4793cc540eef43a46c7a3ce097bd0c86fb344c3c
Yury Semikhatsky <yurys@chromium.org>
2015-06-22 11:51:44 +0300

    Fixed compile Errors

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ./Source/chrome/build/gyp_chromium --depth ./Source/chrome/  -Dv8_use_external_startup_data=0 ./Source/v8inspector/v8inspector.gyp -G output_dir=$(pwd)/out
+
+# Build with Goma
+ninja -C out/Release/ v8inspector_main -j1000
