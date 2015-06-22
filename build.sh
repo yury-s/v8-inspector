a91b04d890caf93b39e22499b1ffa955c4f652bf
Yury Semikhatsky <yurys@chromium.org>
2015-06-22 11:54:39 +0300

    build.sh fix

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 ./Source/chrome/build/gyp_chromium --depth ./Source/chrome/  -Dv8_use_external_startup_data=0 ./Source/v8inspector/v8inspector.gyp -G output_dir=$(pwd)/out
-<<<<<<< HEAD
 
 # Build with Goma
 ninja -C out/Release/ v8inspector_main -j1000
-=======
->>>>>>> 1e0bff693debb78150069dbbce19442609fd8db4
